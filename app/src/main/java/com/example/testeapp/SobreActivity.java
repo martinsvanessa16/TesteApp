997060707d496713317c1bb2f3b475882d5dff42
martinsvanessa16 <alves.nessa16@gmail.com>
2023-08-12 18:50:58 -0300

    Criação de janelas

---
+++ app/src/main/java/com/example/testeapp/SobreActivity.java
@@ -0,0 +1,14 @@
+package com.example.testeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
